d61786b49b833eb46615ce8ea18dd050b3e9c011
박겸영 <yong_drg@naver.com>
2023-01-12 16:11:19 +0900

    fix: CORS Error

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:8080' : '/proxy';
+export const BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:8080' : '';
 
 export const api = {
   todos: `${BASE_URL}/todos`,
